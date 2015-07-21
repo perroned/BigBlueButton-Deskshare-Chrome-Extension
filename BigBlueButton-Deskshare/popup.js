fc854ed65f9805b0d8efb95414300603a32fb7d1
perroned <danny.perrone77@gmail.com>
2015-07-21 12:45:40 -0400

    added browser action menu

---
+++ BigBlueButton-Deskshare/popup.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+	// var content = document.getElementById('content');
+	// content.textContent = "BigBlueButton Desksharing Extension";
+});
